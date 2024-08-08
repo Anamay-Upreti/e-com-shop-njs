b0fa36bcc0cceefe680a819c103c2d5eaa35fb55
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-08-08 23:09:21 +0530

    added-images and texts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,21 @@ export default function Home() {
             <div className="absoulte w-28 left-0 -top-20 hidden lg:block ">
               <Image
               src="/snake-1.png"
+              className="w-full"
+              width={1}
+              height={1}
               />
             </div>
+            <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">Your Image on a <span className="bg-green-600 px-2">Custom</span> Phone Case</h1>
+            <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+              Capture your favorite memories with your own, <span className="font-semibold">one-of-one</span> phone case. CaseCobra allows you to protect your memories, not just your phone case.
+            </p>
+
+            <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+              <div className="space-y-2">
+                <li className="flex gap-1.5 items-center text-left"></li>
+              </div>
+            </ul>
           </div>
         </div>
       </MaxWidthWrapper>
