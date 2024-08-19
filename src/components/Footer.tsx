718ba43a18a60d45af0d5126c97ea1d313e443f6
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-08-19 23:33:00 +0530

    adding footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MaxWidthWrapper from './MaxWidthWrapper'
+
+const Footer = () => {
+  return (
+    <footer className="bg-white h-20 relative">
+    <MaxWidthWrapper>
+          <div className='' />
+    </MaxWidthWrapper>
+    </footer>
+  )
+}
+
+export default Footer
